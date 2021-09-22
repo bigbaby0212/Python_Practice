d12ef9509eed25c2ccd138fb5f12f2ae8bd78b38
bigbaby0212 <bluehome8626@gmail.com>
2021-09-22 23:43:51 +0900

    백준 단계별 if 2번

---
+++ print/10.py
@@ -0,0 +1,12 @@
+X = int(input())
+
+if 100 >= X >= 90:
+    print("A")
+elif 89 >= X >= 80:
+    print("B")
+elif 79 >= X >= 70:
+    print("C")
+elif 69 >= X >= 60:
+    print("D")
+elif 59 >= X >= 0:
+    print("F")
